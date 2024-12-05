008c90cc63aa23088decfd034a3dded817f5f9e5
Magda Hermann <m.d.hermannn@gmail.com>
2024-12-05 19:55:54 +0100

    created springboot project, added dependencies

---
+++ src/main/java/com/kiosk/mckiosk/McKioskApplication.java
@@ -0,0 +1,13 @@
+package com.kiosk.mckiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiosk/mckiosk/McKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiosk.mckiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
